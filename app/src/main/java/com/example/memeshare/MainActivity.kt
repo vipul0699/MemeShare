fe2d33a2028c8cbfaf14e6d5181c8c2e1db08ecd
vipul0699 <vipulsingh0699@gmail.com>
2021-05-18 03:23:26 +0530

    Share feature remains

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.memeshare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadMeme()
+
     }
+
+    private fun loadMeme() {
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
+        val imageView: ImageView = findViewById(R.id.meme)
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+                val url = response.getString("url")
+                Glide.with(this).load(url).into(imageView)
+
+            },
+            Response.ErrorListener {
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+
+            }
+        )
+        queue.add(jsonObjectRequest)
+
+
+    }
+
+    fun nextMeme(view: View) {
+        loadMeme()
+    }
+
 }
